e4b67c3e26a55c2d2913068957072579ff3ec383
davidruc <davidarueda18@gmail.com>
2024-01-09 12:53:30 -0500

    feat: :sparkles: adding some functionalities to the chat
    Provide a method to charge the messages existing in a list of message from a provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // usando el atajo material crea un cascaron de la app automáticamente
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/screens/chat/chat_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -10,11 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'yes no app',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme( selectedColor: 5).theme(),
-      home: const ChatScreen(),
+    return MultiProvider(
+      // de esta forma creo los providers a un nivel de scope global
+      providers: [
+        ChangeNotifierProvider(create: (_)=> ChatProvider())
+      ],
+      child: MaterialApp(
+        title: 'yes no app',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme( selectedColor: 5).theme(),
+        home: const ChatScreen(),
+      ),
     );
   }
 }
--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/widgets/chat/her_bubble_message.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_bubble_message.dart';
 import 'package:yes_no_app/presentation/widgets/shared/message_fiel_box.dart';
@@ -14,7 +17,8 @@ class ChatScreen extends StatelessWidget {
         leading: const Padding(
           padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
-            backgroundImage: NetworkImage("https://styles.redditmedia.com/t5_31ihu/styles/communityIcon_g8jhwcay8ieb1.png"),
+            backgroundImage: NetworkImage(
+                "https://styles.redditmedia.com/t5_31ihu/styles/communityIcon_g8jhwcay8ieb1.png"),
           ),
         ),
         title: const Text('Ariana Grande'),
@@ -27,30 +31,33 @@ class ChatScreen extends StatelessWidget {
 
 class _ChatView extends StatelessWidget {
 
-  
   @override
   Widget build(BuildContext context) {
+    //  que instancia de provider necesito? ChatProvider.
+    final chatProvider = context.watch<ChatProvider>();
+    
     //! Es importante encerrar las screens en este widget SafeArea, para que no quede sobre botones o cosas que hacen parte de la aplicació
     return SafeArea(
       child: Padding(
         //? Una forma de crear paddings que no sean globales, o que sean separados es usanso symetric.
-        padding: const EdgeInsets.symmetric( horizontal: 10.0),
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
         child: Column(
           children: [
-            Expanded(child: ListView.builder(
-              itemCount: 100,
+            Expanded(
+                child: ListView.builder(
+              itemCount: chatProvider.messageList.length,
               itemBuilder: (context, index) {
-                return (index % 2 == 0) 
-                ? const HerMessageBubble() : const MyMessageBubble();
+                final message = chatProvider.messageList[index];
+                return (message.fromWho == FromWho.mine)
+                    ? MyMessageBubble( message: message )
+                    : HerMessageBubble();
               },
             )),
             //caja de mensajes
-          const MessageFieldBox(),
-        
+            const MessageFieldBox(),
           ],
         ),
       ),
     );
   }
 }
-
